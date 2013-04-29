1792add15d17749a91f73b8694f8126ad52eb569
Erdélyi <erdelyizsolt@gmail.com>
2013-04-29 13:56:07 +0200

    Search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Book < ActiveRecord::Base
   attr_accessible :author_name, :category, :isbn, :page_count, :publisher_name, :title, :year
 
@@ -6,4 +8,14 @@ class Book < ActiveRecord::Base
   def at_lending?
     lendings.last && !lendings.last.finished?
   end
+
+  scope :search, (lambda do |search_text|
+    ret = scoped
+    search_text.blank? ? ret : ret.where("author_name    LIKE ? OR
+                                          category       LIKE ? OR
+                                          isbn           LIKE ? OR
+                                          publisher_name LIKE ? OR
+                                          title          LIKE ? OR
+                                          year = ?", "%#{search_text}%", "%#{search_text}%", "%#{search_text}%", "%#{search_text}%", "%#{search_text}%", "%#{search_text}%")
+  end)
 end
